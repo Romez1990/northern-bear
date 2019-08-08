6a5478ce1751b93909fc9980266e69a8806cc50d
Romez1990 <Romez1990@yandex.ru>
2019-08-09 00:25:18 +0300

    Make http request to submit application

--- src/components/SubmitApplication.vue
+++ src/components/SubmitApplication.vue
@@ -1,19 +1,31 @@
 <template lang="pug">
   section#submit-application
     h2 Оставить заявку
-    form
-      input(type='text' placeholder='Ваше имя')
-      input(type='phone' placeholder='Телефон')
-      input(type='email' placeholder='E-mail')
-      input(type='text' placeholder='Сообщение')
-      input(type='button' value='Отправить')
+    form(action='/application' method='post' @submit.prevent='submit')
+      input(type='text' name='name' placeholder='Ваше имя')
+      input(type='phone' name='phone' placeholder='Телефон')
+      input(type='email' name='email' placeholder='E-mail')
+      input(type='text' name='message' placeholder='Сообщение')
+      input(type='submit' value='Отправить')
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component
-export default class SubmitApplication extends Vue {}
+export default class SubmitApplication extends Vue {
+  async submit(e: Event): Promise<void> {
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const params = new URLSearchParams(formData);
+    const res = await fetch(form.action, {
+      method: form.method,
+      body: params,
+    });
+    const json = await res.json();
+    console.log(json);
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -30,7 +42,7 @@ form
 input
   outline none
 
-input:not([type='button'])
+input:not([type='submit'])
   margin 18px 0
   padding 18px 29px
   width 419px
@@ -40,7 +52,7 @@ input:not([type='button'])
   font 18px PT Serif, serif
   color #a8a8a8
 
-input[type='button']
+input[type='submit']
   margin 30px 0 94px
   width 284px
   height 47px
@@ -50,4 +62,5 @@ input[type='button']
   font 18px PT Serif, serif
   color #fff
   text-transform uppercase
+  cursor pointer
 </style>
